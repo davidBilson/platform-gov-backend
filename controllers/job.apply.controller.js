f4ed8acc7159424a42963c11aa5fa9b79f7262a4
David Bilson <davidbilson99@gmail.com>
2025-05-15 18:17:36 +0100

    removed job application error

--- controllers/job.apply.controller.js
+++ controllers/job.apply.controller.js
@@ -128,7 +128,7 @@ export const createJobApplication = async (req, res) => {
     }
 
     // Check if job is still active
-    if (job.status !== 'active') {
+    if (job.status !== 'open') {
       return res.status(400).json({ success: false, message: 'This job is no longer accepting applications' });
     }
 
